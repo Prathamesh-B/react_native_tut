7ed2478a45fb8416519ce69348fdcbae54bd58e6
Prathamesh B <55992548+Prathamesh-B@users.noreply.github.com>
2025-01-06 10:41:44 +0530

    Fix collection ID naming and add image data for galleries, agents, reviews, and properties

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -2,14 +2,13 @@ import { Card, FeaturedCard } from "@/components/Cards";
 import Filters from "@/components/Filters";
 import Search from "@/components/Search";
 import icons from "@/constants/icons";
-import images from "@/constants/images";
 import { useGlobalContext } from "@/lib/global-provider";
 import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
-  const {user} = useGlobalContext();
-  
+  const { user } = useGlobalContext();
+
   return (
     <SafeAreaView className="bg-white h-full">
       <FlatList data={[1, 2]}
@@ -23,7 +22,7 @@ export default function Index() {
           <View className="px-5">
             <View className="flex flex-row items-center justify-between mt-5">
               <View className="flex flex-row items-center">
-                <Image source={{uri: user?.avatar}} className="size-12 rounded-full" />
+                <Image source={{ uri: user?.avatar }} className="size-12 rounded-full" />
                 <View className="flex flex-col items-start ml-2 justify-center">
                   <Text className="text-xs font-rubik text-black-100">Good Morning</Text>
                   <Text className="text-base font-rubik-medium text-black-300">{user?.name}</Text>
